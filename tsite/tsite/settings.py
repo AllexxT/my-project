5fe8cdad72050fba519ce333992d72fcc254b098
AllexxT <sanya92t@gmail.com>
2020-05-03 22:13:49 +0300

    mysql implementation

--- tsite/tsite/settings.py
+++ tsite/tsite/settings.py
@@ -90,8 +90,10 @@ WSGI_APPLICATION = 'tsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tsite_db',
+        'USER': 'tsiteuser',
+        'PASSWORD': 'narlaron120'
     }
 }
 
