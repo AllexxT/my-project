582428f44acc8b78ecd15b83a9b91ff51a6a35aa
AllexxT <sanya92t@gmail.com>
2020-05-19 18:29:40 +0300

    enable description in meta tags

--- tsite/frontend/src/components/titleComponent.jsx
+++ tsite/frontend/src/components/titleComponent.jsx
@@ -3,15 +3,15 @@ import { Helmet } from "react-helmet";
 
 const TitleComponent = ({ title, description }) => {
   const defaultTitle = "ЧП Джас - Производство бетонных изделий в Запорожье";
-//   const defaultDescription =
-    // "ЧП Джас - производитель тротуарной плитки, еврозабора, облицовочного блока, бетонных памятников в Запорожье и области";
+  const defaultDescription =
+    "ЧП Джас - производитель тротуарной плитки, еврозабора, облицовочного блока, бетонных памятников в Запорожье и области";
   return (
     <Helmet>
-      <title>{title ? title : defaultTitle}</title>
       <meta
         name="description"
-        // content={description ? description : defaultDescription}
+        content={description ? description : defaultDescription}
       />
+      <title>{title ? title : defaultTitle}</title>
     </Helmet>
   );
 };
