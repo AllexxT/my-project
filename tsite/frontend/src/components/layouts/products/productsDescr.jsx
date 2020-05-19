a544ac8bc734bbd29033d020f9f4e3c271f240d2
AllexxT <sanya92t@gmail.com>
2020-05-19 20:10:52 +0300

    delete pages description

--- tsite/frontend/src/components/layouts/products/productsDescr.jsx
+++ tsite/frontend/src/components/layouts/products/productsDescr.jsx
@@ -2,26 +2,30 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-    background-color: whitesmoke;
-    margin: 20px 10px 10px 10px;
-    padding: 10px 10px 10px 10px;
-    border-left: 5px solid #F3CB55;
-    border-bottom: 5px solid #535252;
-    h1 {
-        padding-bottom: 15px;
-        text-shadow: 1px 1px 1px rgba(0,0,0,0.5);
-    }
-    & ul li {
-        list-style:inside;
-        font-weight: bold;
-    }
-    ul {
-        margin-bottom: 10px;
-    }
+  background-color: whitesmoke;
+  margin: 20px 10px 10px 10px;
+  padding: 10px 10px 10px 10px;
+  border-left: 5px solid #f3cb55;
+  border-top: 5px solid #f3cb55;
+  border-bottom: 5px solid #535252;
+  border-right: 5px solid #535252;
+  h1 {
+    padding-bottom: 15px;
+    text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.5);
+  }
+  & ul li {
+    list-style: inside;
+    font-weight: bold;
+  }
+  ul {
+    margin-bottom: 10px;
+  }
 `;
 
 const ProductsDescr = ({ textOfPage }) => {
-  return <Container dangerouslySetInnerHTML={{ __html: textOfPage }} />;
+  return (
+    textOfPage && <Container dangerouslySetInnerHTML={{ __html: textOfPage }} />
+  );
 };
 
 export default ProductsDescr;
