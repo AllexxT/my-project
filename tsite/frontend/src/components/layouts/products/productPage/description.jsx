852695dd86d10024b72c658771d0d42f5be94bb3
AllexxT <sanya92t@gmail.com>
2020-06-10 12:43:15 +0300

    change html editor from Jodit to Suneditor

--- tsite/frontend/src/components/layouts/products/productPage/description.jsx
+++ tsite/frontend/src/components/layouts/products/productPage/description.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useRef, useEffect } from "react";
-import JoditEditor from "jodit-react";
+// import JoditEditor from "jodit-react";
 import Dscr, { GlobalS } from "./descriptionStyles";
 import { useSelector } from "react-redux";
+import SunEditor, { buttonList } from "suneditor-react";
+import "suneditor/dist/css/suneditor.min.css"; // Import Sun Editor's CSS File
 
 const Description = ({ description, callBack }) => {
-  const authState = useSelector(state => state.authReducer);
-  const editor = useRef(null);
+  const authState = useSelector((state) => state.authReducer);
+  // const editor = useRef(null);
   const [showEditor, setShowEditor] = useState(false);
   const [content, setContent] = useState("");
   useEffect(() => {
@@ -15,21 +17,21 @@ const Description = ({ description, callBack }) => {
     setContent(description);
   }, [authState.user]);
 
-  const config = {
-    readonly: false,
-    // extraButtons: [
-    //   {
-    //     iconURL: SendBtn,
-    //     exec: () => {}
-    //   }
-    // ],
-    toolbarButtonSize: "large",
-    processPasteHTML: "false"
-    // events: {
-    //   keydown: () => {
-    //   }
-    // }
-  };
+  // const config = {
+  // readonly: false,
+  // extraButtons: [
+  //   {
+  //     iconURL: SendBtn,
+  //     exec: () => {}
+  //   }
+  // ],
+  // toolbarButtonSize: "large",
+  // processPasteHTML: "false"
+  // events: {
+  //   keydown: () => {
+  //   }
+  // }
+  // };
   return (
     <GlobalS>
       {/* <Dscr.Btn onClick={() => setShowEditor(!showEditor)}>
@@ -38,13 +40,20 @@ const Description = ({ description, callBack }) => {
       <div dangerouslySetInnerHTML={{ __html: description }} />
       {showEditor && (
         <>
-          <JoditEditor
+          {/*           <JoditEditor
             ref={editor}
             value={content}
             config={config}
             tabIndex={1} // tabIndex of textarea
             onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
             onChange={newContent => {}}
+          /> */}
+          <SunEditor
+            onChange={(newContent) => setContent(newContent)}
+            setContents={description}
+            setOptions={{
+              buttonList: buttonList.complex,
+            }}
           />
           <Dscr.Btn onClick={() => callBack(content)}>Сохранить</Dscr.Btn>
         </>
